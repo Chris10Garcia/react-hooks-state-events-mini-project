8ac263bf26265270a26f73f8c15799016c39127c
Christian Garcia <bones192nd@gmail.com>
2023-03-30 13:29:59 -0400

    annoying lab

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,21 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({tasks, handleDelete}) {
+  const taskComponent = tasks.map(obj => {
+    return(
+      <Task 
+            text = {obj.text} 
+            category = {obj.category} 
+            key = {obj.text} 
+            handleDelete = {handleDelete}
+      />
+    )
+  })
+
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {taskComponent}
     </div>
   );
 }
